ae598da7b95b9edffc4490489c3ef0d721fb38a1
Danny Sanchez <dsanchez910@gmail.com>
2019-08-13 00:23:58 -0400

    re-worked function for bandsintown. Used axios to pull from bandsintown and added .then function for rresponse to pull data

--- liri.js
+++ liri.js
@@ -7,53 +7,25 @@ const spotify = new Spotify(keys.spotify);
 const moment = require('moment');
 moment().format();
 
-var axios = require('axios');
+const axios = require('axios');
 
 const userOption = process.argv[2]; 
 const inputParameter = process.argv[3];
 
 UserInputs(userOption, inputParameter);
 
-function UserInputs (userOption, inputParameter){
-    switch (userOption) {
-    case 'concert-this':
-        showConcertInfo(inputParameter);
-        break;
+switch (userOption) {
+    case "concert-this":
+        concertThis(inputParameter);
 
-    }
-}
+        };
 
-function showConcertInfo(inputParameter){
-    const queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-    request(queryUrl, function(error, response, body) {
-    
-    if (!error && response.statusCode === 200) {
-        var concerts = JSON.parse(body);
-        for (var i = 0; i < concerts.length; i++) {  
-            console.log("**********EVENT INFO*********");  
-            fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");
-            console.log(i);
-            fs.appendFileSync("log.txt", i+"\n");
-            console.log("Name of the Venue: " + concerts[i].venue.name);
-            fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name+"\n");
-            console.log("Venue Location: " +  concerts[i].venue.city);
-            fs.appendFileSync("log.txt", "Venue Location: " +  concerts[i].venue.city+"\n");
-            console.log("Date of the Event: " +  concerts[i].datetime);
-            fs.appendFileSync("log.txt", "Date of the Event: " +  concerts[i].datetime+"\n");
-            console.log("*****************************");
-            fs.appendFileSync("log.txt", "*****************************"+"\n");
-        }
-    } else{
-      console.log('Error occurred.');
-    }
-});}
 
-function showSomeInfo(){
-	fs.readFile('random.txt', 'utf8', function(err, data){
-		if (err){ 
-			return console.log(err);
-		}
-        var dataArr = data.split(',');
-        UserInputs(dataArr[0], dataArr[1]);
-	});
-}
+        function concertThis(inputParameter) {
+            axios.get("https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp"p")
+            .then(function(response) {    
+                for (var i = 0; i < response.data.length; i++) {
+        
+                    const datetime = response.data[i].datetime; 
+                    const dateArr = datetime.split('T'); 
+
